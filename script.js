3c8bbc6432bc4e1bb3026b304a231c212b5d4525
MateusProDev <130576866+MateusProDev@users.noreply.github.com>
2023-07-30 13:15:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,26 @@ var carrossel = document.getElementById('myCarousel');
         carrossel.classList.add('paused');
       }
     });
+
+// Função para exibir a notificação do carrinho
+function exibirNotificacao(produto) {
+  const notificacao = document.getElementById('notificacao-carrinho');
+  const produtoAdicionado = document.getElementById('produto-adicionado');
+  
+  produtoAdicionado.textContent = produto;
+  notificacao.style.display = 'block';
+
+  // Ocultar a notificação após alguns segundos (por exemplo, 3 segundos)
+  setTimeout(() => {
+    notificacao.style.display = 'none';
+  }, 3000);
+}
+
+// Adiciona um ouvinte de evento a todos os botões com a classe "adicionar-ao-carrinho"
+const botoesAdicionarCarrinho = document.querySelectorAll('.adicionar-ao-carrinho');
+botoesAdicionarCarrinho.forEach((botao) => {
+  botao.addEventListener('click', function () {
+    const produto = botao.getAttribute('data-produto');
+    exibirNotificacao(produto);
+  });
+});
